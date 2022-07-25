c3936e6c4f316dfafc0050dd28428969e4ec68e1
Santiago <saljureo@gmail.com>
2022-07-25 08:23:57 -0700

    Used an openweathermap API to input city name and output latitude and longitude. Used a geonames API to input latitude and longitude and output an Olson timezone. Finally used a timeapi API to input a timezone and output time.

--- getDateFromCity.py
+++ getDateFromCity.py
@@ -1,24 +1,27 @@
 import requests
 
-ciudad = "London"
+ciudad = "Lond"
 requerimiento2 = "http://api.openweathermap.org/geo/1.0/direct?q=" + ciudad + "&limit=1&appid=482cdccc3ed270c76a278ba68a95c3bc"
 respuesta2 = requests.get(requerimiento2)
 #print(respuesta2.json())
 #print("\n")
 print("En " + respuesta2.json()[0]['name'] + " la latitud y longitud son: " + str(respuesta2.json()[0]['lat']) + " y " + str(respuesta2.json()[0]['lon']))
-
 print("\n")
 
-requerimiento3 = "http://api.geonames.org/timezone?lat=" + str(respuesta2.json()[0]['lat']) + "&lng=" + str(respuesta2.json()[0]['lon']) + "&username=demo"
+requerimiento3 = "http://api.geonames.org/timezoneJSON?lat=" + str(respuesta2.json()[0]['lat']) + "&lng=" + str(respuesta2.json()[0]['lon']) + "&username=saljureo"
 respuesta3 = requests.get(requerimiento3)
-print(respuesta3.json())
+#print(respuesta3.json())
 
+#print("\n")
+
+print("Su zona Olson es " + respuesta3.json()['timezoneId'] + ".")
 print("\n")
 
 #timeZone = "Europe/Amsterdam"
+timeZone = respuesta3.json()['timezoneId']
 
-#requerimiento = "https://timeapi.io/api/Time/current/zone?timeZone=" + timeZone
-#respuesta = requests.get(requerimiento)
+requerimiento = "https://timeapi.io/api/Time/current/zone?timeZone=" + timeZone
+respuesta = requests.get(requerimiento)
 #print(respuesta.json())
 #print("\n")
-#print("En " + timeZone + " son las " + str(respuesta.json()['hour']) + ":" + str(respuesta.json()['minute']))
+print("En " + timeZone + " son las " + str(respuesta.json()['hour']) + ":" + str(respuesta.json()['minute']))
