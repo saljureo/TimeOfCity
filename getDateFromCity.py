8959918ac0c8f8a8262351b9033fe5f727e358ef
Santiago <saljureo@gmail.com>
2022-07-25 19:37:27 -0700

    Now program receives a city name and then opens a new window with the answer found

--- getDateFromCity.py
+++ getDateFromCity.py
@@ -1,46 +1,52 @@
 import requests
+import tkinter as tk
 from tkinter import *
 from tkinter import Tk
 from tkinter import ttk
+from tkinter.messagebox import showinfo
 
 
 
 def program():
-    ciudad = "Barcelona"
+    #ciudad = "Manizales"
+    ciudad = city_label.get()
     requerimiento2 = "http://api.openweathermap.org/geo/1.0/direct?q=" + ciudad + "&limit=1&appid=482cdccc3ed270c76a278ba68a95c3bc"
     respuesta2 = requests.get(requerimiento2)
-    #print(respuesta2.json())
-    #print("\n")
-    print("En " + respuesta2.json()[0]['name'] + " la latitud y longitud son: " + str(respuesta2.json()[0]['lat']) + " y " + str(respuesta2.json()[0]['lon']))
-    print("\n")
+    
+    respuestaFinal = "Allí la latitud y longitud son: " + str(respuesta2.json()[0]['lat']) + " y " + str(respuesta2.json()[0]['lon'])
 
     requerimiento3 = "http://api.geonames.org/timezoneJSON?lat=" + str(respuesta2.json()[0]['lat']) + "&lng=" + str(respuesta2.json()[0]['lon']) + "&username=saljureo"
     respuesta3 = requests.get(requerimiento3)
-    #print(respuesta3.json())
 
-    #print("\n")
+    respuestaFinal = respuestaFinal + " y su zona Olson es " + respuesta3.json()['timezoneId'] + "."
 
-    print("Su zona Olson es " + respuesta3.json()['timezoneId'] + ".")
-    print("\n")
-
-    #timeZone = "Europe/Amsterdam"
     timeZone = respuesta3.json()['timezoneId']
 
     requerimiento = "https://timeapi.io/api/Time/current/zone?timeZone=" + timeZone
     respuesta = requests.get(requerimiento)
-    #print(respuesta.json())
-    #print("\n")
-    respuestaFinal = "En " + timeZone + " son las " + str(respuesta.json()['hour']) + ":" + str(respuesta.json()['minute'])
+
+    respuestaFinal = " En " + ciudad + " son las " + str(respuesta.json()['time']) + ".\n\n" + respuestaFinal
     print(respuestaFinal)
-    ttk.Label(frm, text = respuestaFinal).grid(column=0, row=2)
 
+    showinfo(title='¡El tiempo fue calculado!', message = respuestaFinal)
 
-root = Tk()
+root = tk.Tk()
+root.geometry("300x150")
+root.resizable(False,False)
+root.title("What time is it on that city?")
+city = tk.StringVar()
+
 frm = ttk.Frame(root, padding=30)
-frm.grid()
-ttk.Label(frm, text="Enter a city and find out its time: ").grid(column=0, row=0)
-ttk.Entry(frm).grid(column=1, row=0)
-#ttk.Button(frm, text="Search", command=root.destroy).grid(column=0, row=1)
-ttk.Button(frm, text="Search", command=program).grid(column=0, row=1)
-ttk.Label(frm).grid(column=0, row=2)
+frm.pack(padx=10, pady=10, fill='x', expand=True)
+
+question_label = ttk.Label(frm, text="Enter a city and find out its time: ")
+question_label.pack(fill='x', expand=True)
+
+city_label = ttk.Entry(frm, textvariable=city)
+city_label.pack(fill='x', expand=True)
+city_label.focus()
+
+search_button = ttk.Button(frm, text="Search", command=program)
+search_button.pack(fill='x', expand=True)
+
 root.mainloop()
